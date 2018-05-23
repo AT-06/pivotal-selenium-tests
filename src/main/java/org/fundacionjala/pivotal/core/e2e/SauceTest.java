72a25b010f9d57a722dde6710c3f16527f0e2963
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-05-23 19:17:56 -0400

    change

--- src/main/java/org/fundacionjala/pivotal/core/e2e/SauceTest.java
+++ src/main/java/org/fundacionjala/pivotal/core/e2e/SauceTest.java
@@ -1,6 +1,7 @@
 package org.fundacionjala.pivotal.core.e2e;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -11,12 +12,12 @@ public class SauceTest {
     private static final String ACCESS_KEY = "SAUCE_ACCESS_KEY";
     private static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 
-    public static void main(String[] args) throws Exception {
+    public static void createDriver(String browser, String version,String os) throws Exception {
 
         DesiredCapabilities caps = DesiredCapabilities.safari();
-        caps.setCapability("platform", "MacOs  Sierra");
-        caps.setCapability("version", "11");
-        caps.setCapability("resolution", "1024x768");
+        caps.setCapability(CapabilityType.BROWSER_NAME, browser);
+        caps.setCapability(CapabilityType.VERSION, version);
+        caps.setCapability(CapabilityType.PLATFORM_NAME, os);
 
 
         WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
