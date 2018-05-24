7c31e47a5884ea33efa02e04464555363428926d
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-05-24 13:03:11 -0400

    fix

--- src/main/java/org/fundacionjala/pivotal/core/e2e/SauceTest.java
+++ src/main/java/org/fundacionjala/pivotal/core/e2e/SauceTest.java
@@ -5,14 +5,25 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ *Class SauceTest.
+ */
 public class SauceTest {
+    private static final String BUILD = System.getenv("BUILD_TAG");
     private static final String USERNAME = "SAUCE_USERNAME";
     private static final String ACCESS_KEY = "SAUCE_ACCESS_KEY";
     private static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 
-    public static void createDriver(String browser, String version,String os) throws Exception {
+    /**
+     * @param browser navigator.
+     * @param version of os.
+     * @param os is a System operative.
+     * @throws MalformedURLException .
+     */
+     void createDriver(final String browser, final String version, final String os) throws MalformedURLException {
 
         DesiredCapabilities caps = DesiredCapabilities.safari();
         caps.setCapability(CapabilityType.BROWSER_NAME, browser);
@@ -20,9 +31,16 @@ public class SauceTest {
         caps.setCapability(CapabilityType.PLATFORM_NAME, os);
 
 
+
+        if (BUILD != null) {
+            caps.setCapability("build", BUILD);
+        }
+
+        // Launch remote browser and set it as the current thread
         WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 
-        
+
+
 
         driver.quit();
     }
