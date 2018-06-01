5c004321625589c8de86b7453b1db49430a30d15
danielditer <30413308+danielditer@users.noreply.github.com>
2018-06-01 15:12:34 -0400

    Added Browserstack support

---
+++ src/main/java/org/fundacionjala/pivotal/core/e2e/Browserstack.java
@@ -0,0 +1,42 @@
+package org.fundacionjala.pivotal.core.e2e;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Class for set capabilities for testing in Browserstack remote driver.
+ */
+public class Browserstack extends Connection implements Browser {
+    /**
+     * @param url .
+     */
+    public static final String URL = "https://"
+            + BROWSERSTACK_USERNAME + ":"
+            + BROWSERSTACK_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    private static final String BROWSER_NAME = "browser";
+    private static final String BROWSER_VERSION = "browser_version";
+    private static final String BROWSER_OS = "os";
+    private static final String BROWSER_OS_VERSION = "os_version";
+    private static final String BROWSER_RESOLUTION = "resolution";
+
+
+    /**
+     * BrowserStack Set url.
+     */
+    public Browserstack() {
+        super(URL);
+    }
+
+    /**
+     * @return .
+     */
+    @Override
+    DesiredCapabilities setCapabilities() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(BROWSER_NAME, PROPERTIES.getBrowserName());
+        caps.setCapability(BROWSER_VERSION, PROPERTIES.getBrowserVersion());
+        caps.setCapability(BROWSER_OS, PROPERTIES.getOSName());
+        caps.setCapability(BROWSER_OS_VERSION, PROPERTIES.getOSVersion());
+        caps.setCapability(BROWSER_RESOLUTION, PROPERTIES.getBrowserResolution());
+        return caps;
+    }
+}
